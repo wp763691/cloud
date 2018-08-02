26cac2d45ae68f1449ea39cefa48a6503c79b42d
jiangliu.wang <jiangliu.wang@synwing.com>
2018-08-02 22:39:52 +0800

    s

--- cloud-demo/src/main/java/com/synwing/cloud/demo/controller/HystrixController.java
+++ cloud-demo/src/main/java/com/synwing/cloud/demo/controller/HystrixController.java
@@ -1,36 +1,25 @@
-
-/**  
-* @Title: HystrixController.java
-* @Package com.synwing.cloud.demo.controller
-* @Description: TODO(用一句话描述该文件做什么)
-* @author jiangliu.wang
-* @date 2018年8月1日
-* @version V1.0  
-*/
 package com.synwing.cloud.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.synwing.cloud.demo.feign.service.CloudUserService;
 import com.synwing.cloud.demo.hystrix.service.UserService;
 
-/**
- * @ClassName: HystrixController
- * @Description: TODO(这里用一句话描述这个类的作用)
- * @author jiangliu.wang
- * @date 2018年8月1日
- *
- */
 @RestController
 @RequestMapping(value="/cloud")
 public class HystrixController {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private CloudUserService cloudUserService;
 	
 	@RequestMapping(value= "/hystrix")
 	public String testHystrix() {
+		System.out.println(cloudUserService.getUser("123"));
 		return userService.getUser("123");
 	} 
 	
--- cloud-user/src/main/java/com/synwing/cloud/user/App.java
+++ cloud-user/src/main/java/com/synwing/cloud/user/App.java
@@ -1,20 +1,13 @@
-package com.synwing.cloud.demo;
+package com.synwing.cloud.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableHystrix
-@EnableHystrixDashboard
 @EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-		
 	}
 }
